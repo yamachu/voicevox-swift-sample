9ad2d563e72b51b6ea0c1cd197c7b63a9007e295
yamachu <yamachu.dev@gmail.com>
2025-01-15 00:54:57 +0900

    Initial Commit

---
+++ voicevox-swift-sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  voicevox-swift-sample
+//
+//  Created by Yusuke Yamada on 2025/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ voicevox-swift-sample/voicevox_swift_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  voicevox_swift_sampleApp.swift
+//  voicevox-swift-sample
+//
+//  Created by Yusuke Yamada on 2025/01/15.
+//
+
+import SwiftUI
+
+@main
+struct voicevox_swift_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
